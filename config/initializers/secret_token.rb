e3311c1e5150c30eb32a051219886be75fd1acdb
Jason Atwood <jwatwood@gmail.com>
2013-01-12 19:38:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ToolShare::Application.config.secret_token = 'be190419857218fc2c7e53b37b9b50a35044c7f1914ecefa58993820514971d15faea55fa93b7f3b888cd10ecdc05fb7862990e63c01a5b1e2194669b3b6369b'
