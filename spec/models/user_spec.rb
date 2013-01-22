e6972f9c00ed05f5a4be7ef21a6cea1e73d5605b
Jason Atwood <jwatwood@gmail.com>
2013-01-21 23:42:06 -0500

    adds User model, db, and spec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe User do
+
+  before (:each) do
+    @testUser = FactoryGirl.create(:user)
+  end
+
+  subject(@testUser)
+
+  it{ should respond_to(:name)}
+
+  it "should be valid" do
+    @testUser.should be_valid
+  end
+
+  it "should not be valid without name" do
+    @testUser.name = ""
+    @testUser.should_not be_valid
+  end
+end
