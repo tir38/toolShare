0106e7dfd0cdfe5e4d0b8c0899867a88a3a0ade9
Jason Atwood <jwatwood@gmail.com>
2013-06-01 12:38:55 -0400

    adds spec for signup URL and cleans up spec/views/home_page_spec

--- spec/views/home_page_spec.rb
+++ spec/views/home_page_spec.rb
@@ -1,12 +1,14 @@
 require 'spec_helper'
 
 describe "home page" do
+
+  subject {page}
+
   before do
     visit '/' # visit root
   end
 
-  it "should display correct text" do
-    page.should have_content("This is the static home page.")
+  it {should have_content("This is the static home page.")}
+  it {should have_link('Sign Up', href:signup_path)}
 
-  end
 end
